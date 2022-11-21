d0480fbe68f04b938e7f4ce2dfef29ff6d1b8a1c
Gokulraaju <gokulraaju@gmail.com>
2022-11-21 09:23:13 +0530

    msg

---
+++ spring.java
@@ -0,0 +1,9 @@
+public class spring{
+    public static main(String[] args){
+        //Spring boot is framework of java 
+        //We use it to create a application
+        // it purely based on dependency injection
+        // It has juint and mockito framework
+        //it has spring security dependency too
+    }
+}
